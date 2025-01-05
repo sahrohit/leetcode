d1877a0bd440e1a32e8ac5eff14ca1dd5db48d38
Rohit Sah <sahrohit9586@gmail.com>
2025-01-05 00:16:05 -0600

    Time: 52 ms (72.56%) | Memory: 51.2 MB (27.80%) - LeetSync

--- 278-first-bad-version/first-bad-version.ts
+++ 278-first-bad-version/first-bad-version.ts
@@ -8,15 +8,21 @@
 var solution = function (isBadVersion: any) {
 
     return function (n: number): number {
+
+        // [1......n] Since the list is already sorted, we can use binary search
         let l = 0, r = n;
 
+        // Loop until the left pointer hasn't crossed right
         while (l < r) {
+
+            // Checking the floor and ceil of the mid value on the divider line
             let [midFloor, midCeil] = [Math.floor((l + r) / 2), Math.ceil((l + r) / 2)]
 
             if (!isBadVersion(midFloor) && isBadVersion(midCeil)) {
                 return midCeil;
             }
 
+            // Shortening the search boundary by changing the left & right pointer
             if (isBadVersion(midFloor)) {
                 r = midFloor;
             } else {
