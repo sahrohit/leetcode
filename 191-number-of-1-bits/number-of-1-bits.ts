c9e305ce3dc301c652d3b2d232c3aa4fcc271861
Rohit Sah <sahrohit9586@gmail.com>
2025-01-04 20:12:42 -0600

    Time: 1 ms (43.99%) | Memory: 52.2 MB (9.07%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.ts
+++ 191-number-of-1-bits/number-of-1-bits.ts
@@ -1,3 +1,5 @@
 function hammingWeight(n: number): number {
+    // (n).toString(2) will convert the number to binary string (only +ve numbers)
+    // Then we split the array and take the sum of it using .reduce function
     return (n).toString(2).split("").reduce((a, x) => Number(a) + Number(x), 0);
 };
