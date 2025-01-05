1d9e8f73243c51feaf68c4e7c4c667f1cb913da8
Rohit Sah <sahrohit9586@gmail.com>
2025-01-05 00:56:08 -0600

    Time: 9 ms (9.62%) | Memory: 54.4 MB (5.77%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.ts
+++ 1983-maximum-population-year/maximum-population-year.ts
@@ -1,1 +1,50 @@
-function maximumPopulation(t: number[][]): number { let e = {}, o = {}; for (let n of t) e[n[0]] ? e[n[0]]++ : e[n[0]] = 1, o[n[1]] ? o[n[1]]++ : o[n[1]] = 1; let l = [...new Set(t.flat().sort((t, e) => t - e))], i = 0, r = t[0][0], $ = 0; for (let f of l) Object.keys(e).includes(f.toString()) && ($ += e[f]), Object.keys(o).includes(f.toString()) && ($ -= o[f]), $ > i && (i = $, r = f); return r }
+function maximumPopulation(logs: number[][]): number {
+    let births = {};
+    let deaths = {};
+
+    // Taking frequency count of all births and deaths
+    for (const log of logs) {
+        if (births[log[0]]) {
+            births[log[0]]++
+        } else {
+            births[log[0]] = 1
+        }
+
+        if (deaths[log[1]]) {
+            deaths[log[1]]++
+        } else {
+            deaths[log[1]] = 1
+        }
+    }
+
+    // Creating a timeline consisitng of all birth and deaths years for a sweepline plane
+    const timeline = [...new Set(logs.flat().sort((a, b) => a - b))]
+
+    // Keeping track of current population, max and max year
+    let max = 0, maxYear = logs[0][0]
+    let population = 0;
+
+    // Sweeping Line with a year
+    for (const year of timeline) {
+
+        // If the current sweeping line has any year included in births
+        if (Object.keys(births).includes(year.toString())) {
+            population += births[year]
+        }
+
+        // If the current sweeping line has any year included in deaths
+        if (Object.keys(deaths).includes(year.toString())) {
+            population -= deaths[year];
+        }
+
+        // Updating the max and maxYear if max population is found 
+        if (population > max) {
+            max = population;
+
+            // Also updating the year for max population
+            maxYear = year;
+        }
+    }
+
+    return maxYear;
+};
