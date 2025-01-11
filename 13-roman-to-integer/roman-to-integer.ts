212416cc3a087a09ba1a371de29f88ccb245828c
Rohit Sah <sahrohit9586@gmail.com>
2025-01-11 08:00:53 -0600

    Time: 7 ms (58.88%) | Memory: 56.7 MB (79.37%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.ts
@@ -0,0 +1,24 @@
+function romanToInt(s: string): number {
+    const ROMAN = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+
+    let result = 0;
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (ROMAN[s[i - 1]] < ROMAN[s[i]]) {
+            result += (ROMAN[s[i]] - ROMAN[s[i - 1]])
+            i--;
+        } else {
+            result += ROMAN[s[i]]
+        }
+    }
+
+    return result;
+};
