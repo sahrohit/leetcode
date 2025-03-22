27640a2792d01ea1051d4d69760dce133b832fe4
Rohit Sah <sahrohit9586@gmail.com>
2025-03-22 13:39:32 -0500

    Time: 6 ms (48.29%) | Memory: 58.2 MB (23.24%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,11 +1,24 @@
 function twoSum(nums: number[], target: number): number[] {
 
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = 0; j < nums.length; j++) {
+    //         if (i !== j && nums[i] + nums[j] === target) {
+    //             return [i, j]
+    //         }
+    //     }
+    // }
+    // return [0, 0]
+
+    const targets = {}
+
+    nums.forEach((x, i) => {
+        targets[target - x] = i
+    })
+
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++) {
-            if (i !== j && nums[i] + nums[j] === target) {
-                return [i, j]
-            }
+        if (Number.isFinite(targets[nums[i]]) && targets[nums[i]] !== i) {
+            return [targets[nums[i]], i]
         }
     }
-    // return [0, 0]
+
 };
