91082602ce009cf28ac6799fcef0743d17462cef
Rohit Sah <sahrohit9586@gmail.com>
2025-03-22 13:26:59 -0500

    Time: 119 ms (9.14%) | Memory: 56.9 MB (40.43%) - LeetSync

---
+++ 1-two-sum/two-sum.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i !== j && nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+    // return [0, 0]
+};
